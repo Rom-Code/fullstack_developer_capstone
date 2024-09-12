d30c9ac5e449ea2673dcf236a1727ad55a4a7c08
Rom-Code <57846275+Rom-Code@users.noreply.github.com>
2024-09-12 19:22:41 +0300

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,7 +12,8 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'],
+                                                         description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -33,7 +34,7 @@ def initiate():
        "car_make": car_make_instances[1]},
       {"name": "C-Class",
        "type": "SUV",
-       "year": 2023, "car_make":car_make_instances[1]},
+       "year": 2023, "car_make": car_make_instances[1]},
       {"name": "E-Class",
        "type": "SUV",
        "year": 2023,
@@ -72,12 +73,12 @@ def initiate():
        "car_make": car_make_instances[4]},
       {"name": "Kluger",
        "type": "SUV",
-         "year": 2023,
-         "car_make": car_make_instances[4]},
+        "year": 2023,
+        "car_make": car_make_instances[4]},
         # Add more CarModel instances as needed
-    ]
-
+        ]
+    
     for data in car_model_data:
-        CarModel.objects.create(name=data['name'], 
-                                car_make=data['car_make'], 
+        CarModel.objects.create(name=data['name'],
+                                car_make=data['car_make'],
                                 type=data['type'], year=data['year'])
