223738e227706d155efd4200ac3fa6ca0a71bada
Rom-Code <57846275+Rom-Code@users.noreply.github.com>
2024-09-12 20:06:08 +0300

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -144,7 +144,6 @@ MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
