cb180b90199b02afa8e492648b37cdf2e74e3c0b
Rom-Code <57846275+Rom-Code@users.noreply.github.com>
2024-09-12 13:23:09 +0300

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,8 +12,7 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-            description=data['description']))
+        car_make_instances.append(CarMake.objects.create(name=data['name'],description=data['description']))
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -36,17 +35,19 @@ def initiate():
        "type": "SUV",
        "year": 2023, "car_make":car_make_instances[1]},
       {"name": "E-Class",
-       "type": "SUV", "year": 2023,
-       "car_make":car_make_instances[1]},
+       "type": "SUV",
+       "year": 2023,
+       "car_make": car_make_instances[1]},
       {"name": "A4",
-       "type":"SUV", "year": 2023,
+       "type": "SUV",
+       "year": 2023,
        "car_make": car_make_instances[2]},
       {"name": "A5",
-       "type":"SUV",
+       "type": "SUV",
        "year": 2023,
        "car_make": car_make_instances[2]},
       {"name": "A6",
-       "type":"SUV",
+       "type": "SUV",
        "year": 2023,
        "car_make": car_make_instances[2]},
       {"name": "Sorrento",
