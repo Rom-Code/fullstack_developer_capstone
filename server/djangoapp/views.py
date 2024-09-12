21f28bd8dde34a88792c173389763af70d0b806a
Rom-Code <57846275+Rom-Code@users.noreply.github.com>
2024-09-12 20:51:16 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -130,6 +130,6 @@ def add_review(request):
         except Exception as e:
             logger.error(f"Error posting review: {e}")
             return JsonResponse({"status": 401,
-            "message": "Error in posting review"})
+                                "message": "Error in posting review"})
 
     return JsonResponse({"status": 403, "message": "Unauthorized"})
