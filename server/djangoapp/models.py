275d387876f79a49d5d3507306850ef526333d63
Rom-Code <57846275+Rom-Code@users.noreply.github.com>
2024-09-12 12:47:52 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,7 +3,7 @@ Uncomment the following imports before adding the Model code
 """
 
 from django.db import models
-#from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -14,8 +14,10 @@ class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 # - Any other fields you would like to include in car make model
+    
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     """
@@ -30,10 +32,11 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,validators=[
+    year = models.IntegerField(default=2023, validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
+
     # Other fields as needed
 
     def __str__(self):
