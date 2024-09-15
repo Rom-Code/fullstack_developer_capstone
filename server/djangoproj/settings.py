473db9a5887d05ae3243249289e31a949fa86b46
rom-code <raad.cars@gmail.com>
2024-09-15 02:38:10 -0400

    deploy Kupernetes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,23 +31,21 @@ DEBUG = True
 """
 ALLOWED_HOSTS = [
     'localhost',
-    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
 ]
 """
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai'
+    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.'
-    'proxy.cognitiveclass.ai'
+    'https://u4husam-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/'
 ]
 
 REST_FRAMEWORK = {
